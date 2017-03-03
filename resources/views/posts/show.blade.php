4c2468a655b75110fa78c99d000e53db8bdf35d6
jverg <giannisverg@gmail.com>
2017-03-03 00:25:00 -0800

    Deletion of vendor and creation of edit post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,10 +7,44 @@
 
 @section('content')
 
-   <p class="lead">This is the blog post</p>
+    <div class="row">
+        <div class="col-md-8">
+            <!-- Post title -->
+            <h1>{{ $post->title }}</h1>
 
-@endsection
+            <!-- Post body -->
+            <p class="lead">{{ $post->body }}</p>
+        </div>
 
-@section('scripts')
-    {!! Html::script('js/parsley.js') !!}}
+        <div class="col-md-4">
+            <div class="well">
+                <!-- Created at element -->
+                <dl class="dl-horizontal">
+                    <dt>Created at:</dt>
+                    <dd>{{ date('M j, Y h:ia', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <!-- Updated at element -->
+                <dl class="dl-horizontal">
+                    <dt>Updated at:</dt>
+                    <dd>{{ date('M j, Y h:ia', strtotime($post->updated_at)) }}</dd>
+                </dl>
+                <hr>
+
+                <!-- Delete and Edit buttons -->
+                <div class="row">
+
+                    <!-- Edit button -->
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+                    </div>
+
+                    <!-- Delete button -->
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
