837c4b07a044cd37a027dd83da6a86c0887c9275
Jverg <giannisverg@gmail.com>
2017-03-05 13:15:26 -0800

    Remove the redirects_to '/home' and add some <br> tags

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -10,7 +10,7 @@
 <!-- Main content of the post -->
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
-        <h1>{{ $post->title }}</h1>
+        <h1>{{ $post->title }}</h1><br>
         <h4>{{ date('M j, Y', strtotime($post->created_at)) }}</h4>
         <p>{!! $post->body !!}</p>
     </div>
