bf8e93dd3e15959c86c2f43d0496f8dd77e7711f
jverg <giannisverg@gmail.com>
2017-03-07 10:04:41 -0800

    Initial commit with user page

--- resources/views/profile/my_profile.blade.php
+++ resources/views/profile/my_profile.blade.php
@@ -7,7 +7,32 @@
 
 @section('content')
 
-    <p>{{ $user->name }}</p>
-    <p>{{ $user->email }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4">
+                            <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" />
+                        </div>
+                        <div class="col-sm-6 col-md-8">
+                            <h2>{{ $user->name }}</h2><hr>
+                            <p>
+                                <h4>Social</h4>
+                                <i class="fa fa-facebook-official"></i><a href="#"> Facebook</a><br>
+                                <i class="fa fa-twitter"></i><a href="#"> Twitter</a>
+                            </p><hr>
+                            <p>
+                                <h5><i class="glyphicon glyphicon-map-marker"></i>   San Francisco, USA</h5>
+                                <h5><i class="glyphicon glyphicon-envelope"></i>   {{ $user->email }}</h5>
+                                <h5><i class="glyphicon glyphicon-gift"></i>   June 02, 1988</h5>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 @endsection
